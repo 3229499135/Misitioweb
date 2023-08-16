0fe99a1a15bbbe7ddb903e245640d4d939705e83
josea348 <jv1424937@gmail.com>
2023-08-16 16:18:52 -0500

    the fonts

--- js/script.js
+++ js/script.js
@@ -1,87 +1,21 @@
-(async function (d) {
-  //Fecha formato humano
-  function humanDate(date) {
-    return new Date(`${date}T00:00:00`)
-      .toDateString()
-      .slice(4)
-      .replace("Jan", "Ene")
-      .replace("Apr", "Abr")
-      .replace("Aug", "Ago")
-      .replace("Dec", "Dic");
-  }
+/* date = new Date();
+year = date.getFullYear();
+month = date.getMonth()+1;
+day = date.getDate();
+document.getElementById("date").innerHTML = day+" / "+month+" / "+year;
 
-  //Datos del sitio
-  async function getData() {
-    let res = await fetch("assets/data.json"),
-      json = await res.json();
-    return json;
-  }
 
-  let data = await getData();
+let tarbajos = document.getElementById("tarbajos");
+let practicas = document.getElementById("practicas");
+let menuUno = document.querySelector("ul");
 
-  //Artículos del blog
-  if (d.getElementById("posts")) {
-    const posts = data.posts.filter((el) => el.publish === true);
-    let $posts = "";
-
-    posts.forEach(
-      (el) =>
-        ($posts += `
-          <a href="${location.origin}/${el.slug}" class="blog-item">
-            <figure>
-              <img src="img/category/${el.category}.svg" alt="${
-          el.category
-        }" title="Categoría: ${el.category}" loading="lazy">
-              <figcaption>
-                <span>${el.title}</span>
-                <time datetime="${el.date}">${humanDate(el.date)}</time>
-              </figcaption>
-            </figure>
-          </a>
-      `)
-    );
-    d.getElementById("posts").insertAdjacentHTML("beforeend", $posts);
-  }
-  
-  //Artículo o nota actual
-  if (d.getElementById("post-blog")) {
-    let post;
-
-    const currentPost = data.posts.filter(
-      (el) =>
-        location.pathname === `/${el.slug}` ||
-        location.pathname === `/${el.slug}.html`
-    );
-
-    if (currentPost.length === 0) {
-      const currentToughts = data.thoughts.filter(
-        (el) =>
-          location.pathname === `/${el.slug}` ||
-          location.pathname === `/${el.slug}.html`
-      );
-
-      post = currentToughts[0];
-    } else {
-      post = currentPost[0];
-    }
-
-    d.getElementById("post-blog").innerHTML = `
-        <h1>${post.title}</h1>
-        <aside class="post-date">
-          <small>
-            <span>Última actualización:</span>
-            <i>
-              <time datetime="${post.date}">
-                ${humanDate(post.date)}
-              </time>
-            </i>
-          </small>
-        </aside>
-        <img class="post-category" src="img/category/${
-          post.category
-        }.svg" alt="Categoría: ${post.category}" title="Categoría: ${
-      post.category
-    }" loading="lazy">
-      `;
-  }
-})(document);
+tarbajos.addEventListener("click", function() {
+  menuUno.classList.add("menu-1");
+  menuUno.classList.remove("menu-1-1");
+});
+practicas.addEventListener("click", function() {
+  menuUno.classList.remove("menu-1");
+  menuUno.classList.add("menu-1-1");
+  menuUno.classList.remove("menu-2");
+  menuUno.classList.add("menu-2-2");
+}); */
