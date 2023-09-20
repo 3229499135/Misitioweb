a1a4961f5a194fe20d4fa22aeb7669d505cc8fc5
josea348 <jv1424937@gmail.com>
2023-09-20 16:55:18 -0500

    add fonts

--- js/slider.js
+++ js/slider.js
@@ -2,6 +2,21 @@ let boxSlider = document.getElementById("boxSlider");
 let iconchevronRight = document.getElementById("iconchevronRight");
 let iconChevronLeft = document.getElementById("iconChevronLeft");
 
+iconchevronRight.addEventListener.apply("click", function() {
+    if (boxSlider.classList.contains("box-slide-1")) {
+        boxSlider.classList.remove("box-slide-1");
+        boxSlider.classList.add("box-slide-2");
+    }
+    else if (boxSlider.classList.contains("box-slide-2")) {
+        boxSlider.classList.remove("box-slide-2");
+        boxSlider.classList.add("box-slide-3");
+    }
+    else if (boxSlider.classList.contains("box-slide-3")) {
+        boxSlider.classList.remove("box-slide-3");
+        boxSlider.classList.add("box-slide-4");
+    }
+})
+
 boxSlider.addEventListener("click", function() {
     if (boxSlider.classList.contains("box-slide-1")) {
         boxSlider.classList.remove("box-slide-1");
