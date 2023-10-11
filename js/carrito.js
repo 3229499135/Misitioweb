1efb53e23614c1694863fca5c4a61c4ef7cfa424
TatianaVargasD <tatianavd03@gmail.com>
2023-10-11 15:14:42 -0500

    fonts

--- js/carrito.js
+++ js/carrito.js
@@ -1,13 +1,20 @@
 let boxMinus = document.getElementById("boxMinus");
 let boxCantidad = document.getElementById("boxCantidad");
 let boxPlus = document.getElementById("boxPlus");
-let subtotal = document.getElementById("subtotal");
+let valorProducto = 2000000;
+let auxValorProducto = new Intl.NumberFormat("de-DE").format(valorProducto);
+document.getElementById('precioUnidad').innerHTML = auxValorProducto;
+let subtotal = document.getElementById('subtotal');
 
 boxPlus.addEventListener("click", function() {
     let auxCant = boxCantidad.innerHTML;
     if(auxCant < 10) {
         auxCant++;
         boxCantidad.innerHTML = auxCant;
+        total = auxValorProducto * boxCantidad;
+        if (isNaN(total)) {
+            subtotal.innerHTML = total;
+        }
     }
 });
 
