c819edfd9daa8d00fc02b17ebcfab972668c1687
josea348 <jv1424937@gmail.com>
2023-12-13 16:11:41 -0500

    add code

--- js/amazon.js
+++ js/amazon.js
@@ -464,11 +464,11 @@ auriculares3.addEventListener('click', function(){
 // Fin de las funciones para los divs de las imagenes con los precios
 
 // Relleno de las barras de calificacion
-let  number5 = 68254;
-let  number4 = 15512;
-let  number3 = 8273;
-let  number2 = 4137;
-let  number1 = 7239;
+/* const  number5 = 68254;
+const  number4 = 15512;
+const  number3 = 8273;
+const  number2 = 4137;
+const  number1 = 7239;
 
 // Las barras
 let barraStar5 = document.getElementById("barraStar5");
@@ -489,31 +489,33 @@ let textBarra3 = document.getElementById("textBarra3");
 let textBarra2 = document.getElementById("textBarra2");
 let textBarra1 = document.getElementById("textBarra1");
 
-graficar.addEventListener("click", function() {
+function actualizarBarras() {
     // Grafica 5
-    let valorTotalCalificacion = parseInt(number5) + parseInt(number4) + parseInt(number3) + parseInt(number2) + parseInt(number1);
-    let numberStar5 = (number5 * 100 / valorTotalCalificacion);
+    let valorTotalCalificacion = parseInt(number5.value) + parseInt(number4.value) + parseInt(number3.value) + parseInt(number2.value) + parseInt(number1.value);
+    let numberStar5 = (number5.value * 100 / valorTotalCalificacion);
     barraStar5.style.width = numberStar5.toString()+"%";
     textBarra5.innerText = numberStar5.toFixed()+"%";
 
     // Grafica 4
-    let numberStar4 = (number4 * 100 / valorTotalCalificacion);
+    let numberStar4 = (number4.value * 100 / valorTotalCalificacion);
     barraStar4.style.width = numberStar4.toString()+"%";
     textBarra4.innerText = numberStar4.toFixed()+"%";
     
     // Grafica 3
-    let numberStar3 = (number3 * 100 / valorTotalCalificacion);
+    let numberStar3 = (number3.value * 100 / valorTotalCalificacion);
     barraStar3.style.width = numberStar3.toString()+"%";
     textBarra3.innerText = numberStar3.toFixed()+"%";
     
     // Grafica 2
-    let numberStar2 = (number2 * 100 / valorTotalCalificacion);
+    let numberStar2 = (number2.value * 100 / valorTotalCalificacion);
     barraStar2.style.width = numberStar2.toString()+"%";
     textBarra2.innerText = numberStar2.toFixed()+"%";
     
     // Grafica 1
-    let numberStar1 = (number1 * 100 / valorTotalCalificacion);
+    let numberStar1 = (number1.value * 100 / valorTotalCalificacion);
     barraStar1.style.width = numberStar1.toString()+"%";
     textBarra1.innerText = numberStar1.toFixed()+"%";
-});
+};
+
+actualizarBarras; */
 // end of the bars
--- js/barras-de-estrellas.js
+++ js/barras-de-estrellas.js
@@ -1,9 +1,9 @@
-// Los inputs
-let  number5 = document.getElementById("number5");
-let  number4 = document.getElementById("number4");
-let  number3 = document.getElementById("number3");
-let  number2 = document.getElementById("number2");
-let  number1 = document.getElementById("number1");
+// Relleno de las barras de calificación
+const number5 = document.getElementById("number5");
+const number4 = document.getElementById("number4");
+const number3 = document.getElementById("number3");
+const number2 = document.getElementById("number2");
+const number1 = document.getElementById("number1");
 
 // Las barras
 let barraStar5 = document.getElementById("barraStar5");
@@ -12,20 +12,13 @@ let barraStar3 = document.getElementById("barraStar3");
 let barraStar2 = document.getElementById("barraStar2");
 let barraStar1 = document.getElementById("barraStar1");
 
-let numberStar5 = 0;
-let numberStar4 = 0;
-let numberStar3 = 0;
-let numberStar2 = 0;
-let numberStar1 = 0;
-let graficar = document.getElementById("graficar");
-
 let textBarra5 = document.getElementById("textBarra5");
 let textBarra4 = document.getElementById("textBarra4");
 let textBarra3 = document.getElementById("textBarra3");
 let textBarra2 = document.getElementById("textBarra2");
 let textBarra1 = document.getElementById("textBarra1");
 
-graficar.addEventListener("click", function() {
+function actualizarBarras() {
     // Grafica 5
     let valorTotalCalificacion = parseInt(number5.value) + parseInt(number4.value) + parseInt(number3.value) + parseInt(number2.value) + parseInt(number1.value);
     let numberStar5 = (number5.value * 100 / valorTotalCalificacion);
@@ -51,4 +44,11 @@ graficar.addEventListener("click", function() {
     let numberStar1 = (number1.value * 100 / valorTotalCalificacion);
     barraStar1.style.width = numberStar1.toString()+"%";
     textBarra1.innerText = numberStar1.toFixed()+"%";
-});
+}
+
+// Agregar eventos "input" a cada input
+number5.addEventListener("input", actualizarBarras);
+number4.addEventListener("input", actualizarBarras);
+number3.addEventListener("input", actualizarBarras);
+number2.addEventListener("input", actualizarBarras);
+number1.addEventListener("input", actualizarBarras);
