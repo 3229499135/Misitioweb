ed9e7633c15aa4a25df80f2ea2518f6bedfc21af
josea348 <jv1424937@gmail.com>
2023-12-13 16:48:06 -0500

    add code

--- js/amazon.js
+++ js/amazon.js
@@ -464,24 +464,19 @@ auriculares3.addEventListener('click', function(){
 // Fin de las funciones para los divs de las imagenes con los precios
 
 // Relleno de las barras de calificacion
-/* const  number5 = 68254;
-const  number4 = 15512;
-const  number3 = 8273;
-const  number2 = 4137;
-const  number1 = 7239;
+// Relleno de las barras de calificacion
+const number5 = 68254;
+const number4 = 15512;
+const number3 = 8273;
+const number2 = 4137;
+const number1 = 7239;
 
-// Las barras
+// Las barras del primer conjunto
 let barraStar5 = document.getElementById("barraStar5");
 let barraStar4 = document.getElementById("barraStar4");
 let barraStar3 = document.getElementById("barraStar3");
 let barraStar2 = document.getElementById("barraStar2");
 let barraStar1 = document.getElementById("barraStar1");
-
-let numberStar5 = 0;
-let numberStar4 = 0;
-let numberStar3 = 0;
-let numberStar2 = 0;
-let numberStar1 = 0;
 
 let textBarra5 = document.getElementById("textBarra5");
 let textBarra4 = document.getElementById("textBarra4");
@@ -489,33 +484,49 @@ let textBarra3 = document.getElementById("textBarra3");
 let textBarra2 = document.getElementById("textBarra2");
 let textBarra1 = document.getElementById("textBarra1");
 
+// Las barras del segundo conjunto
+let barraInterior5 = document.querySelector(".barra-ineterior-5");
+let barraInterior4 = document.querySelector(".barra-ineterior-4");
+let barraInterior3 = document.querySelector(".barra-ineterior-3");
+let barraInterior2 = document.querySelector(".barra-ineterior-2");
+let barraInterior1 = document.querySelector(".barra-ineterior-1");
+
+let textBarraGlobal5 = document.getElementById("textBarraGlobal5");
+let textBarraGlobal4 = document.getElementById("textBarraGlobal4");
+let textBarraGlobal3 = document.getElementById("textBarraGlobal3");
+let textBarraGlobal2 = document.getElementById("textBarraGlobal2");
+let textBarraGlobal1 = document.getElementById("textBarraGlobal1");
+
 function actualizarBarras() {
     // Grafica 5
-    let valorTotalCalificacion = parseInt(number5.value) + parseInt(number4.value) + parseInt(number3.value) + parseInt(number2.value) + parseInt(number1.value);
-    let numberStar5 = (number5.value * 100 / valorTotalCalificacion);
-    barraStar5.style.width = numberStar5.toString()+"%";
-    textBarra5.innerText = numberStar5.toFixed()+"%";
+    let valorTotalCalificacion = number5 + number4 + number3 + number2 + number1;
+    numberStar5 = (number5 * 100 / valorTotalCalificacion);
+    barraStar5.style.width = numberStar5.toString() + "%";
+    textBarra5.innerText = numberStar5.toFixed() + "%";
 
     // Grafica 4
-    let numberStar4 = (number4.value * 100 / valorTotalCalificacion);
-    barraStar4.style.width = numberStar4.toString()+"%";
-    textBarra4.innerText = numberStar4.toFixed()+"%";
-    
-    // Grafica 3
-    let numberStar3 = (number3.value * 100 / valorTotalCalificacion);
-    barraStar3.style.width = numberStar3.toString()+"%";
-    textBarra3.innerText = numberStar3.toFixed()+"%";
-    
-    // Grafica 2
-    let numberStar2 = (number2.value * 100 / valorTotalCalificacion);
-    barraStar2.style.width = numberStar2.toString()+"%";
-    textBarra2.innerText = numberStar2.toFixed()+"%";
-    
-    // Grafica 1
-    let numberStar1 = (number1.value * 100 / valorTotalCalificacion);
-    barraStar1.style.width = numberStar1.toString()+"%";
-    textBarra1.innerText = numberStar1.toFixed()+"%";
-};
+    numberStar4 = (number4 * 100 / valorTotalCalificacion);
+    barraStar4.style.width = numberStar4.toString() + "%";
+    textBarra4.innerText = numberStar4.toFixed() + "%";
 
-actualizarBarras; */
+    // Grafica 3
+    numberStar3 = (number3 * 100 / valorTotalCalificacion);
+    barraStar3.style.width = numberStar3.toString() + "%";
+    textBarra3.innerText = numberStar3.toFixed() + "%";
+
+    // Grafica 2
+    numberStar2 = (number2 * 100 / valorTotalCalificacion);
+    barraStar2.style.width = numberStar2.toString() + "%";
+    textBarra2.innerText = numberStar2.toFixed() + "%";
+
+    // Grafica 1
+    numberStar1 = (number1 * 100 / valorTotalCalificacion);
+    barraStar1.style.width = numberStar1.toString() + "%";
+    textBarra1.innerText = numberStar1.toFixed() + "%";
+}
+
+// Llama a la función para inicializar las barras
+actualizarBarras();
+
+document.getElementById("calificacionGlobal").style.transition = "opacity 0.5s ease-in-out, max-height 0.5s ease-in-out";
 // end of the bars
